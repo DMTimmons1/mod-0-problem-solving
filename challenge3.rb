4059acb0755eacfc231243bb7fdbeabf49b13ca7
Dawson Timmons <DawsonTimmons@gmail.com>
2022-11-04 12:04:45 -0600

    Add array and iteration for challenge3

---
+++ challenge3.rb
@@ -0,0 +1,14 @@
+# Challenge 3
+# Start with an array of travel destinations. Print every travel destination in alphabetical 
+# order embedded in a sentence using string interpolation. For example, if the destination is 
+# "New York City", print something like "The next place I want to visit is New York City!" 
+
+locations = ["Denver", "Chigago", "New York City", "Los Angeles", "Detroit", "Las Vegas"]
+
+locations.sort.each do |location|
+    puts "The next place I want to visit is #{location}!"
+end
+
+
+
+
