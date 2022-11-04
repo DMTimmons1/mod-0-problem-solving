22732bba1d428c05c6a067372a3370cc53129cbd
Dawson Timmons <DawsonTimmons@gmail.com>
2022-11-04 12:17:12 -0600

    Add thought process for challenge3

--- challenge3.rb
+++ challenge3.rb
@@ -3,6 +3,15 @@
 # order embedded in a sentence using string interpolation. For example, if the destination is 
 # "New York City", print something like "The next place I want to visit is New York City!" 
 
+# start with creating an array! Look at each element and determine if they are already in alphabetical
+# order. 
+
+# need an arry of strings (multiple different locations)
+# look at each string -- .each method?
+# need to sort by alphabetical order -- .sort method.
+# add our puts statement after modifiers so we  will print alphabeically and with the 
+# string interpolation to create our statement!
+
 locations = ["Denver", "Chigago", "New York City", "Los Angeles", "Detroit", "Las Vegas"]
 
 locations.sort.each do |location|
