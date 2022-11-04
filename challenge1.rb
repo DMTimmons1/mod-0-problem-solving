718e1eaa1ae3ca3f5791b9510564cc74dd8e0da7
Dawson Timmons <DawsonTimmons@gmail.com>
2022-11-04 12:09:40 -0600

    Add thought process for challenge 1

--- challenge1.rb
+++ challenge1.rb
@@ -1,6 +1,16 @@
 # challenge 1:
 # Given an array of strings, return only the strings that have exactly 4 characters.
 
+# Start with an array of strings. Then, print only the words with exatly 4 characters.
+
+# start with creaing an array and ook at each element to check if it's exactly characters. If the element has more than 3 characters. It will print.
+# If the element has exatly 4 characters. It will print those elements.
+
+# need an array with strings (some with exatcly 4 characters, some with more, and some with less) 
+# look at each string -- .each method?
+# if length of word is == 4 print the word -- .length method.
+
+
 names = ["Dawson", "Cody", "Joseph", "Rick", "Sam", "Matt"]
 
 names.each do |name|
